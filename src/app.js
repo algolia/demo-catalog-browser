13792f194f72104f897b065817d097dc9c5151ca
Alphonse Bouy <alphonse.bouy@gmail.com>
2020-06-29 12:32:34 +0200

    clean + style

--- src/app.js
+++ src/app.js
@@ -3,13 +3,49 @@ import { customCarousel } from './widgets/Carousel';
 /* global algoliasearch instantsearch */
 const searchClient = algoliasearch(
   'GENYJWQIK2',
-  'd7a56394e700ad117ef483c12bc04481'
+  '61a10f4c69aee59a993ebc3021574681'
 );
 
 const search = instantsearch({
   indexName: 'perso_movies_carousel',
   searchClient,
 });
+
+// const movies = searchClient.initIndex('perso_movies_carousel');
+
+// movies
+//   .search('', {
+//     filters: 'genre:action AND genre:crime',
+//     hitsPerPage: 100,
+//   })
+//   .then(({ hits }) => {
+//     aa('clickedObjectIDs', {
+//       index: 'perso_movies_carousel',
+//       eventName: 'CLICKED_MOVIE',
+//       userToken: 'action_crime_fan',
+//       objectIDs: hits
+//         .filter(() => Math.floor(Math.random() * 10) < 7)
+//         .slice(43, 61)
+//         .map(hit => hit.objectID),
+//     });
+
+//     aa('viewedObjectIDs', {
+//       index: 'perso_movies_carousel',
+//       eventName: 'VIEWED_MOVIE',
+//       userToken: 'action_crime_fan',
+//       objectIDs: hits.slice(65, 87).map(hit => hit.objectID),
+//     });
+
+//     aa('convertedObjectIDs', {
+//       index: 'perso_movies_carousel',
+//       eventName: 'WATCHED_MOVIE',
+//       userToken: 'action_crime_fan',
+//       objectIDs: hits
+//         .filter(() => Math.floor(Math.random() * 10) < 3)
+//         .slice(1, 19)
+//         .map(hit => hit.objectID),
+//     });
+//   });
 
 const referenceHit = {
   title: 'The Imitation Game',
@@ -25,114 +61,160 @@ const referenceHit = {
   objectID: '439434880',
 };
 
-const myCarousels = [
-  {
-    title: 'Trending',
-    context: 'trending',
-    indexName: 'perso_movies_carousel_trending',
-  },
-  {
-    title: 'Most Popular',
-    context: 'most-popular',
-    indexName: 'perso_movies_carousel',
-  },
-  {
-    title: 'Last chance',
-    context: 'last-chance',
-    indexName: 'perso_movies_carousel_last_chance',
-  },
-  {
-    title: 'You started watching',
-    context: 'ongoing',
-    indexName: 'perso_movies_carousel',
-    extraConfig: {
-      facetFilters: 'ongoing_watch: user_1',
-      hitsPerPage: 5,
-    },
-  },
-  {
-    title: 'Because you watched The Imitation Game',
-    context: 'related',
-    indexName: 'perso_movies_carousel_related',
-    relatedRef: referenceHit,
-  },
-  {
-    title: 'Fantasy Comedy',
-    context: 'comedy_fantasy',
-    indexName: 'perso_movies_carousel',
-  },
-];
+const searchParams = new URLSearchParams(window.location.search);
 
-search.addWidgets(
-  myCarousels.map(carousel =>
-    instantsearch.widgets
-      .index({
-        indexName: carousel.indexName,
-        indexId: carousel.context,
-      })
-      .addWidgets(
-        !carousel.relatedRef
-          ? [
-              instantsearch.widgets.configure({
-                ruleContexts: carousel.context,
-                hitsPerPage: 15,
-                query: '',
-                ...carousel.extraConfig,
-              }),
-              customCarousel({
-                container: '.carousels',
-                title: carousel.title,
-                context: carousel.context,
-              }),
-            ]
-          : [
-              instantsearch.widgets.configure({
-                ruleContexts: carousel.context,
-                hitsPerPage: 15,
-                query: '',
-                ...carousel.extraConfig,
-              }),
-              instantsearch.widgets.EXPERIMENTAL_configureRelatedItems({
-                hit: carousel.relatedRef,
-                matchingPatterns: {
-                  genre: { score: 3 },
-                  actors: { score: 2 },
-                },
-              }),
-              customCarousel({
-                container: '.carousels',
-                title: carousel.title,
-                context: carousel.context,
-              }),
-            ]
-      )
-  )
-);
+const userToken = searchParams.get('userToken');
+
+const myCarousels = userToken
+  ? [
+      {
+        title: 'Trending',
+        context: 'trending',
+        indexName: 'perso_movies_carousel_trending',
+      },
+      {
+        title: 'Most Popular',
+        context: 'most-popular',
+        indexName: 'perso_movies_carousel',
+      },
+      {
+        title: 'Last chance',
+        context: 'last-chance',
+        indexName: 'perso_movies_carousel_last_chance',
+      },
+
+      {
+        title: 'Fantasy Comedy',
+        context: 'comedy_fantasy',
+        indexName: 'perso_movies_carousel',
+      },
+      {
+        title: 'You started watching',
+        context: 'ongoing',
+        indexName: 'perso_movies_carousel',
+        extraConfig: {
+          facetFilters: 'ongoing_watch: user_1',
+          hitsPerPage: 5,
+        },
+      },
+      {
+        title: 'Because you watched The Imitation Game',
+        context: 'related',
+        indexName: 'perso_movies_carousel_related',
+        relatedRef: referenceHit,
+      },
+      {
+        title: 'Curated for you',
+        context: 'curated',
+        indexName: 'perso_movies_carousel',
+        extraConfig: {
+          enablePersonalization: true,
+          userToken,
+        },
+      },
+    ]
+  : [
+      {
+        title: 'Trending',
+        context: 'trending',
+        indexName: 'perso_movies_carousel_trending',
+      },
+      {
+        title: 'Most Popular',
+        context: 'most-popular',
+        indexName: 'perso_movies_carousel',
+      },
+      {
+        title: 'Last chance',
+        context: 'last-chance',
+        indexName: 'perso_movies_carousel_last_chance',
+      },
+
+      {
+        title: 'Fantasy Comedy',
+        context: 'comedy_fantasy',
+        indexName: 'perso_movies_carousel',
+      },
+    ];
+
+const addWidgets = () => {
+  search.addWidgets(
+    myCarousels.map(carousel =>
+      instantsearch.widgets
+        .index({
+          indexName: carousel.indexName,
+          indexId: carousel.context,
+        })
+        .addWidgets(
+          !carousel.relatedRef
+            ? [
+                instantsearch.widgets.configure({
+                  ruleContexts: carousel.context,
+                  hitsPerPage: 15,
+                  query: '',
+                  ...carousel.extraConfig,
+                }),
+                customCarousel({
+                  container: '.carousels',
+                  title: carousel.title,
+                  context: carousel.context,
+                }),
+              ]
+            : [
+                instantsearch.widgets.configure({
+                  ruleContexts: carousel.context,
+                  hitsPerPage: 15,
+                  query: '',
+                  ...carousel.extraConfig,
+                }),
+                instantsearch.widgets.EXPERIMENTAL_configureRelatedItems({
+                  hit: carousel.relatedRef,
+                  matchingPatterns: {
+                    genre: { score: 3 },
+                    actors: { score: 2 },
+                  },
+                }),
+                customCarousel({
+                  container: '.carousels',
+                  title: carousel.title,
+                  context: carousel.context,
+                }),
+              ]
+        )
+    )
+  );
+};
+
+addWidgets();
 
 search.start();
 
-document.addEventListener('click', e => {
-  if (e.target.matches('.analytics')) {
-    console.log('click', e.target.dataset.objectId);
+// document.querySelector('#user_select').addEventListener('change', e => {
+//   window.location.href = `${window.location.href}?userToken=${e.currentTarget.value}`;
+// });
 
-    aa('clickedObjectIDs', {
-      index: 'perso_movies_carousel',
-      eventName: 'CLICKED_MOVIE',
-      objectIDs: [e.target.dataset.objectId],
-    });
+// document.addEventListener('click', e => {
+//   if (e.target.matches('.analytics')) {
+//     console.log('click', e.target.dataset.objectId);
 
-    aa('viewedObjectIDs', {
-      index: 'perso_movies_carousel',
-      eventName: 'VIEWED_MOVIE',
-      objectIDs: [e.target.dataset.objectId],
-    });
+//     aa('clickedObjectIDs', {
+//       index: 'perso_movies_carousel',
+//       eventName: 'CLICKED_MOVIE',
+//       objectIDs: [e.target.dataset.objectId],
+//     });
 
-    if (Math.floor(Math.random() * 10) < 4) {
-      aa('convertedObjectIDs', {
-        index: 'perso_movies_carousel',
-        eventName: 'WATCHED_MOVIE',
-        objectIDs: [e.target.dataset.objectId],
-      });
-    }
-  }
-});
+//     aa('viewedObjectIDs', {
+//       index: 'perso_movies_carousel',
+//       eventName: 'VIEWED_MOVIE',
+//       objectIDs: [e.target.dataset.objectId],
+//     });
+
+//     if (Math.floor(Math.random() * 10) < 4) {
+//       aa('convertedObjectIDs', {
+//         index: 'perso_movies_carousel',
+//         eventName: 'WATCHED_MOVIE',
+//         objectIDs: [e.target.dataset.objectId],
+//       });
+//     }
+//   }
+// });
--- src/widgets/Carousel.js
+++ src/widgets/Carousel.js
@@ -22,30 +22,6 @@ const renderCarousel = ({ widgetParams, hits }, isFirstRender) => {
 
     wrapper.style.width = `${window.innerWidth -
       wrapper.getBoundingClientRect().left}px`;
-
-    const leftArrow = document.createElement('button');
-    leftArrow.innerHTML =
-      '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-arrow-right-circle"><circle cx="12" cy="12" r="10"></circle><polyline points="12 16 16 12 12 8"></polyline><line x1="8" y1="12" x2="16" y2="12"></line></svg>';
-    wrapper.appendChild(leftArrow);
-    const rightArrow = document.createElement('button');
-    rightArrow.innerHTML =
-      '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-arrow-right-circle"><circle cx="12" cy="12" r="10"></circle><polyline points="12 16 16 12 12 8"></polyline><line x1="8" y1="12" x2="16" y2="12"></line></svg>';
-    rightArrow.style.transform = 'rotate(180deg)';
-    rightArrow.style.display = 'none';
-    rightArrow.style.right = 'auto';
-    rightArrow.style.left = '16px';
-    wrapper.appendChild(rightArrow);
-
-    leftArrow.addEventListener('click', () => {
-      carouselListContainer.scroll(wrapper.clientWidth, 0);
-      leftArrow.style.display = 'none';
-      rightArrow.style.display = 'block';
-    });
-    rightArrow.addEventListener('click', () => {
-      carouselListContainer.scroll(0, 0);
-      rightArrow.style.display = 'none';
-      leftArrow.style.display = 'block';
-    });
   }
 
   const ul = container.querySelector(`ul#${widgetParams.context}`);
@@ -58,16 +34,18 @@ const renderCarousel = ({ widgetParams, hits }, isFirstRender) => {
           : ''
       }">
             <img src="${hit.image}" alt="${hit.title}">
-            <h3>${hit.title}</h3>
-            <ul>
+            <div class="overlay">
+              <h3>${hit.title}</h3>
+              <ul>
                 ${hit.genre
                   .map(
                     genre => `
-                    <li>${genre}</li>
+                  <li>${genre}</li>
                 `
                   )
                   .join('')}
-            </ul>
+              </ul>
+          </div>
         </li>
     `
     )
